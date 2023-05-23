a918d63584a2a67b6a72ccb49f4e5d128e924efd
grig <prowlergrig@yahoo.ie>
2023-05-23 12:51:00 +0300

    add HC-SR04 distance sensor

---
+++ sensorA/ipconfig.h
@@ -0,0 +1,13 @@
+#ifndef _IP_CONFIG_H_
+#define _IP_CONFIG_H_
+
+/* The Arduino's IP. Use commas to separate IP values instead of dots */
+#define SELF_IP 192,168,0,5
+
+/* Put the host IP here. */
+#define HOST_IP 192,168,0,104
+
+/* Put the UDP port where MAX/MSP is listening for OSC messages */
+#define HOST_PORT 9999
+
+#endif
