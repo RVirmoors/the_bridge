c704e5ce373cb6c6226bec2e46a423e060dbf9c1
grig <prowlergrig@yahoo.ie>
2023-05-23 13:28:14 +0300

    pump send PWM

--- pump/pump.ino
+++ pump/pump.ino
@@ -3,6 +3,8 @@
 #include <SPI.h>
 #include <OSCMessage.h>
 
+#define PWM_PIN 5
+
 #include "ipconfig.h"
 // https://forum.arduino.cc/t/finding-the-mac-address-from-ethernet-shield-w5100/203506
 byte mac[] = {  
@@ -16,15 +18,16 @@ IPAddress ip(SELF_IP);
 // port number
 const unsigned int inPort = IN_PORT;
 
-
 void setup() {
   Serial.begin(9600);
+  // pinMode(PWM_PIN, OUTPUT);
   
   Ethernet.begin(mac,ip);
   udp.begin(IN_PORT);
 }
 
 void loop() {
+  float duty = 0.0f;
   if (udp.parsePacket()) {
     while (udp.available()) {
       // Read the incoming OSC message
@@ -38,11 +41,11 @@ void loop() {
       Serial.println("Invalid or unexpected OSC message");
     } else if (oscMessage.isFloat(0)) {
       // Get the float value from the OSC message
-      float value = oscMessage.getFloat(0);
+      duty = oscMessage.getFloat(0);
 
       // Print the received PWM value to the serial monitor
       Serial.print("Received PWM value: ");
-      Serial.println(value, 4);  // Print with 4 decimal places
+      Serial.println(duty, 4);  // Print with 4 decimal places
     } else {
       // Invalid data type for the value
       Serial.println("Invalid data type for the PWM value");
@@ -51,4 +54,7 @@ void loop() {
     // Clear the OSC message for the next iteration
     oscMessage.empty();
   }
+
+  duty *= 255.f;
+  analogWrite(PWM_PIN, (int)duty);
 }
